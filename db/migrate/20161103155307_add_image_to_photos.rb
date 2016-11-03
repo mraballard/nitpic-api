b53170660e1c850822ffa734e64dffb3efa6eb30
estermer <eric.stermer@gmail.com>
2016-11-03 11:55:23 -0400

    ran rails db:mgrate to add image_file_name, image_file_size, image_content_type and image_update_at to the photos model

--- db/migrate/20161103155307_add_image_to_photos.rb
+++ db/migrate/20161103155307_add_image_to_photos.rb
@@ -1,4 +1,9 @@
 class AddImageToPhotos < ActiveRecord::Migration[5.0]
-  def change
+  def self.up
+    add_attachment :photos, :image
+  end
+
+  def self.down
+    remove_attachment :photos, :image
   end
 end

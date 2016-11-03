5a4dc95a04f91e7f81aa907eed37624c3a691550
estermer <eric.stermer@gmail.com>
2016-11-02 21:48:42 -0400

    Added routes for users, user login, albums, photos, and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create, :show, :update, :destroy] do
+    resources :albums, only: [:index, :show, :create, :update, :destroy] do
+      resources :photos, only: [:index, :show, :create, :destroy] do
+        resources :comments, only: [:index, :create, :destroy]
+      end
+    end
+
+    collection do
+      post '/login', to: 'users#login'
+    end
+  end
+  # resources :albums do
+  #   resources :photos
+  # end
+  # resources :photos do
+  #   resources :comments, only: [:index, :update, :destroy]
+  # end
+  # resources :comments, only: [:update, :destroy]
 end

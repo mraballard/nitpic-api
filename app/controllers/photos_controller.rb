803806743a192b703104325127fb8024cb36b662
estermer <eric.stermer@gmail.com>
2016-11-03 15:29:28 -0400

    added permit params for the the photo to require :title, image file name, image content type, image file size, and image update at and then commented it all out for testing putposes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,10 +3,6 @@ class PhotosController < ApplicationController
     render json: Album.find(params[:album_id]).photos
   end
 
-  def new
-    @photo = Photo.new
-  end
-
   def create
     @photo = Photo.new(photo_params)
     if photo.save
@@ -27,7 +23,13 @@ class PhotosController < ApplicationController
   end
 
   private
-    def photo_params
-      params.required(:photo).permit(:title, :album_id)
-    end
+    # def photo_params
+    #   params.required(:photo).permit(
+    #     :title,
+    #     :image_file_name,
+    #     :image_content_type,
+    #     :image_file_size,
+    #     :image_updated_at
+    #   )
+    # end
 end

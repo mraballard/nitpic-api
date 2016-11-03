e56f965f72df65aece679df3af796c7356cbdda2
estermer <eric.stermer@gmail.com>
2016-11-03 11:51:36 -0400

    Added image file attribute to the photo model
    validated the content type of the attachment as the desired file type

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,12 @@
 class Photo < ApplicationRecord
   belongs_to :album, dependent: :destroy
   has_many :comments
+  has_attached_file :image, styles: {
+    thumb: '200x200>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end

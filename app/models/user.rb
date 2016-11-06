685059f2cb1977f3dd6c813a4d7b978fdf29d815
Aaron Ballard <mraballard@gmail.com>
2016-11-06 16:43:13 -0300

    Added migration to add user_id as foreign_key for comments. Change user model and comment model to include has_many and belongs_to accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :albums, dependent: :destroy
+  has_many :comments
   validates :username, presence: true, uniqueness: true
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103155307) do
+ActiveRecord::Schema.define(version: 20161106194040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,9 @@ ActiveRecord::Schema.define(version: 20161103155307) do
     t.integer  "photo_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.index ["photo_id"], name: "index_comments_on_photo_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
   create_table "photos", force: :cascade do |t|
@@ -57,5 +59,6 @@ ActiveRecord::Schema.define(version: 20161103155307) do
 
   add_foreign_key "albums", "users"
   add_foreign_key "comments", "photos"
+  add_foreign_key "comments", "users"
   add_foreign_key "photos", "albums"
 end

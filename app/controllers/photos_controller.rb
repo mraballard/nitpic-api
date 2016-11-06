680e1454849e8d481aab326f09a1a92eb1b149f0
Aaron Ballard <mraballard@gmail.com>
2016-11-06 14:35:19 -0300

    added server.pid to gitignore, changed photo#show method to call correct method on Photo model

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  before_action :set_album, only: [:index, :show]
+  before_action :set_album, only: [:index]
 
   def index
     set_album
@@ -24,7 +24,7 @@ class PhotosController < ApplicationController
   def show
     photo = Photo.find(params[:id])
 
-    render json: {status: 200, photo: photo, source: photo.get_photo_url}
+    render json: {status: 200, photo: photo, source: photo.get_photo_data}
   end
 
   def destroy
